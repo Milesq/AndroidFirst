37cc6f24d4c8f6f8e9b95381e0812915de87a0f9
MrPhilip7 <72625726+MrPhilip7@users.noreply.github.com>
2021-03-09 20:43:14 +0100

    Przypisanie działania Button_1/2

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            val Bt1 = findViewById<Button>(R.id.Button_1)
+            val Bt2 = findViewById<Button>(R.id.Button_2)
+
+            Bt1.setOnClickListener{
+                var message = Toast.makeText(applicationContext, "Thank You", Toast.LENGTH_LONG)
+                message.show()
+        }
+
+            Bt2.setOnClickListener{
+                var message = Toast.makeText(applicationContext, "Congratulations", Toast.LENGTH_SHORT)
+                message.show()
+            }
+
+
     }
 }
